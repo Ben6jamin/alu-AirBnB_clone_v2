42d6262ff687aac0c22ef645eaf83200cdfbb654
Kaluval <v.kalu@alustudent.com>
2023-06-15 12:37:33 +0200

    test

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -25,7 +25,7 @@ class test_User(test_basemodel):
 
     def test_email(self):
         """ """
-        new = self.value(email='test@gmail.com')
+        new = self.value()
         self.assertEqual(type(new.email), str)
 
     def test_password(self):
