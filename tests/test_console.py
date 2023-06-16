9ccd3849139f010e5e0c8d8201a6991132cda6ae
Ben6jamin <b.ishimwe1@alustudent.com>
2023-06-16 14:13:23 -0700

    test

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,4 +1,7 @@
+#!/usr/bin/python3
+"""Test for console"""
 import unittest
+
 from console import HBNBCommand
 from unittest.mock import patch
 from io import StringIO
@@ -6,9 +9,7 @@ import models
 
 
 class ConsoleTestCase(unittest.TestCase):
-    """
-    Test for console
-    """
+    """Test for console"""
 
     def setUp(self):
         self.console = HBNBCommand()
@@ -19,65 +20,70 @@ class ConsoleTestCase(unittest.TestCase):
         del self.stdout
         del self.storage
 
-    @unittest.skip("Skipping test_create")
     def test_create(self):
-        """
-        test create basic
-        """
-        pass
+        """test create basic"""
+        with patch('sys.stdout', self.stdout):
+            self.console.onecmd('create State')
+        state_id = self.stdout.getvalue()[:-1]
+        # print(state_id)
+        # print(len(state_id))
+        self.assertTrue(len(state_id) == 36)
 
     def test_create_save(self):
-        """
-        test create save
-        """
+        """test create save"""
         with patch('sys.stdout', self.stdout):
-            self.console.onecmd('create State name="California"')
+            self.console.onecmd('create State name="California')
         state_id = self.stdout.getvalue()[:-1]
         self.assertIsNotNone(
             self.storage.all()["State.{}".format(state_id)])
 
     def test_create_non_existing_class(self):
-        """
-        test non-existing class
-        """
+        """test non-existing class"""
         with patch('sys.stdout', self.stdout):
             self.console.onecmd('create MyModel')
-        self.assertEqual("** class doesn't exist **\n", self.stdout.getvalue())
+        self.assertEqual("** class doesn't exist **\n",
+                         self.stdout.getvalue())
 
-    @unittest.skip("Skipping test_all")
     def test_all(self):
-        """
-        test all
-        """
-        pass
+        """test all"""
+        with patch('sys.stdout', self.stdout):
+            self.console.onecmd('create State name="California"')
+        with patch('sys.stdout', self.stdout):
+            self.console.onecmd('all State')
+        output = self.stdout.getvalue()[:-1]
+        self.assertIn("State", output)
+        self.assertIn("California", output)
 
-    @unittest.skip("Skipping test_update")
     def test_update(self):
-        """
-        test update
-        """
-        pass
+        with patch('sys.stdout', self.stdout):
+            self.console.onecmd('create State name="California"')
+        state_id = self.stdout.getvalue()[:-1]
+        with patch('sys.stdout', self.stdout):
+            self.console.onecmd(
+                'update State {} name="New California"'.format(state_id))
+        with patch('sys.stdout', self.stdout):
+            self.console.onecmd('show State {}'.format(state_id))
+        output = self.stdout.getvalue()[:-1]
+        self.assertIn("California", output)
+
+    def test_destroy(self):
+        """test destroy"""
+        with patch('sys.stdout', self.stdout):
+            self.console.onecmd('create State name="California"')
+        state_id = self.stdout.getvalue()[:-1]
+        with patch('sys.stdout', self.stdout):
+            self.console.onecmd('destroy State {}'.format(state_id))
+        # with patch('sys.stdout', self.stdout):
+        #     self.console.onecmd('show State {}'.format(state_id))
+        # self.assertEqual("** no instance found **\n",
+        #                  self.stdout.getvalue())
 
     def test_show(self):
-        """
-        test show
-        """
+        """test show"""
         with patch('sys.stdout', self.stdout):
             self.console.onecmd('create State name="California"')
         state_id = self.stdout.getvalue()[:-1]
         with patch('sys.stdout', self.stdout):
             self.console.onecmd('show State {}'.format(state_id))
-        self.assertIn("California", self.stdout.getvalue())
-
-    def test_new_test_case(self):
-        """
-        Test a new functionality in Console
-        """
-        with patch('sys.stdout', self.stdout):
-            self.console.onecmd('help quit')
-        self.assertIn("Exits the program with formatting",
-                      self.stdout.getvalue())
-
-
-if __name__ == '__main__':
-    unittest.main()
+        output = self.stdout.getvalue()[:-1]
+        self.assertIn("California", output)
